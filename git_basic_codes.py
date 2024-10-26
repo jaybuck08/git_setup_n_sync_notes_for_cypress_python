ca57f1231653eb3a6b25e06fbf2a143c1a8db2c4
jaybuck08 <felix.jr.njoku@gmail.com>
2024-10-26 13:24:12 +0100

    now

---
+++ git_basic_codes.py
@@ -0,0 +1,29 @@
+
+
+
+# To link tests to your GIT account:
+
+# create new repository in git e.g "ecommerce_testing" and copy the link
+# Go to VS code and type "git init" in the treminal of the test or file you want to link
+# click source control (left side of screen) and click the tree radio buttons/dots at the top (more actions)
+# select remote & add remote, select the remote locationfrom the dropdown that appears at the top, give it a name "e.g origin".  This automatically connects your repositiry to vs code
+# commit (always add a comment) and publish branch. Go back to your repository in git and referesh to see.
+
+# type "git branch Felix" '  (note that "Felix is the branch name & can be renamed anything") in the terminal to create new branch or
+# click source control and click the tree radio buttons/dots above then click "branch" - "create branch"
+
+
+# setting global username and email for git in vs code:
+
+# git config --global user.name    example username - "John Doe"
+# git config --global user.email   example email - "johndoe@example.com"
+
+# git init  :initailses the git in your system
+# git add . :helps you add a folder or file from a directory
+
+
+# one more time again
+
+
+# git rm -r --cached  : used to clear the cache for files on vs code that have already been pushed to git
+# .env   : these files are used to save secert keys e.g. passwords for your eyes only.
